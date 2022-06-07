8fdf674e40cbf26c0353d8796829130bfc6d7713
Daniel B <dbranquinhogomes@gmail.com>
2022-06-07 16:00:10 -0300

    Update grafico

--- new-proj/src/app/app.module.ts
+++ new-proj/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AngAppComponent } from './ang-app/ang-app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AngAppComponent } from './ang-app/ang-app.component';
     AngAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
