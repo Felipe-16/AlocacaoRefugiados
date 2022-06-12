df45cd3d360329a0974d9eb96b2d9365a4a2cf36
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2022-06-12 16:54:09 -0300

    Arrumando routing de abrigos

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbrigoComponent } from './abrigo/abrigo.component';
 import { ExtratoDoacaoComponent } from './extrato-doacao/extrato-doacao.component';
 import { TransferenciaDoacaoComponent } from './transferencias-doacao/transferencias-doacao.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'abrigo', pathMatch: 'full'},
+  {path: 'abrigo', component: AbrigoComponent},
   {path: 'extrato-doacao', component: ExtratoDoacaoComponent},
   {path: 'transferencias-doacao', component: TransferenciaDoacaoComponent},
 ];
