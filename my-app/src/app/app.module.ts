e484a3a4f493408f5440ec746b3f5dccef7eefbb
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2022-06-12 23:58:50 -0300

    Arrunmando routing + module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import localePt from '@angular/common/locales/pt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExtratoDoacaoComponent } from './extrato-doacao/extrato-doacao.component';
+import { TransferenciaDoacaoComponent } from './transferencias-doacao/transferencias-doacao.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ExtratoDoacaoComponent, TransferenciaDoacaoComponent,HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
@@ -24,4 +31,4 @@ registerLocaleData(localePt, 'pt');
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
