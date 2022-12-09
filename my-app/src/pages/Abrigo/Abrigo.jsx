a7ac899beb6c671b70a48d0059cfba5e3be3b80e
Felipe Ros Pegini <felipe16699@gmail.com>
2022-12-09 01:06:48 -0300

    Arrumando page view de abrigo

--- my-app/src/pages/Abrigo/Abrigo.jsx
+++ my-app/src/pages/Abrigo/Abrigo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
 import "./abrigo.css";
 
 class Abrigo extends Component {
@@ -6,35 +7,21 @@ class Abrigo extends Component {
         return (
             <ul className="abrigos">
                 <li>
-                    <h2>Abrigo 1</h2>
-                    <img src="https://www.educolorir.com/foto-campo-de-refugiados-paquist-o-dl28908.jpg"/>
+                    <h2 className="titulo-li">Abrigo 1</h2>
+                    <div className="media"></div>
+                    <div className="texto-descricao">
                     <p className="descricao_abrigo">Abrigo 1 fica localizado na provincia de São Paulo.</p>
-                    <p className="info_abrigo"> Para mais Informações</p>
+                    <p className="descricao_abrigo">Já foram realocadas 10 pessoas nesse abrigo.</p>
+                    <p className="descricao_abrigo">Total de doações feitas para esse abrigo: R$1500.</p>
+
+
+                    <p className="doar-link"><Link to = '/transferencia'><p>Para realizar uma doação</p></Link></p>
+                    </div>
+                    
                 </li>
-                <li>
-                    <h2>Abrigo 2</h2>
-                    <img src="https://www.educolorir.com/foto-campo-de-refugiados-paquist-o-dl28908.jpg"/>
-                    <p className="descricao_abrigo">Abrigo 2 fica localizado na provincia de São Paulo.</p>
-                    <p className="info_abrigo"> Para mais Informações</p>
-                </li>
-                <li>
-                    <h2>Abrigo 3</h2>
-                    <img src="https://www.educolorir.com/fo2o-campo-de-refugiados-paquist-o-dl28908.jpg"/>
-                    <p className="descricao_abrigo">Abrigo 3 fica localizado na provincia de São Paulo.</p>
-                    <p className="info_abrigo"> Para mais Informações</p>
-                </li>
-                <li>
-                    <h2>Abrigo 4</h2>
-                    <img src=""/>
-                    <p className="descricao_abrigo">Abrigo 1.....</p>
-                    <p className="info_abrigo"> Para mais Informações</p>
-                </li>
-                <li>
-                    <h2>Abrigo 5</h2>
-                    <img src=""/>
-                    <p className="descricao_abrigo">Abrigo 1.....</p>
-                    <p className="info_abrigo"> Para mais Informações</p>
-                </li>
+
+                
+                
             </ul>
 
         )
