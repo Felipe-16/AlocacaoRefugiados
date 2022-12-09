e810d4003cf2b9f5d8bdc5aff57d1c14597bc87d
Guilherme Okushi <guiokushi@hotmail.com>
2022-12-09 16:22:54 -0300

    update barramento de eventos

--- Backend/transferencias/src/routes.ts
+++ Backend/transferencias/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import DoacaoController from "./controller/DoacaoController";
 
-//Rotas para GET e POST
+//Rotas para GET e POST de doação
 const routes = Router();
 routes.get("/donations", DoacaoController.find);
 routes.post("/donate", DoacaoController.create);
+
+routes.post("/", )
 
 export default routes;
