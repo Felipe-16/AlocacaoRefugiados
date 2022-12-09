3adba34adfc9f127d17508ce45a55f9515e9f6f4
Felipe Ros Pegini <felipe16699@gmail.com>
2022-12-08 22:14:39 -0300

    Arrumando pagina 404

--- my-app/src/pages/Pagina404/Pagina404.jsx
+++ my-app/src/pages/Pagina404/Pagina404.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import "./pag404.css";
+import { Link } from 'react-router-dom'
 
-const Pagina404 = () => { 
+
+const Pagina404 = () => {
   return (
-    <main>
-      <p>
+    <main className='main-404'>
+      <h1 className='error'>ERROR 404</h1>
+      <p className='msg-error'>
         Ops, Essa página não existe!
       </p>
+
+      <div className='container-botao'>
+      <h2 className='voltar'><Link to = '/'>Voltar para o Inicio</Link></h2>
+      </div>
+      
     </main>
   )
 }
 
-export default Pagina404
+export default Pagina404;
